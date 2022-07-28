352202893c78e75ee08067e9185351dea2b40dcb
Filipe Sales <eduardojs999@gmail.com>
2022-07-28 11:08:50 -0300

    slideshow finished

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 'use strict';
 
 const images = [
-    { 'id': '1', 'url':'1.png'},
-    { 'id': '2', 'url':'2.png'},
-    { 'id': '3', 'url':'3.png'},
-    { 'id': '4', 'url':'4.png'},
+    { 'id': '1', 'url':'chrono.jpg'},
+    { 'id': '2', 'url':'inuyasha.jpg'},
+    { 'id': '3', 'url':'tenchi.jpg'},
+    { 'id': '4', 'url':'tenjhotenge.jpg'},
+    { 'id': '5', 'url':'yuyuhakusho.jpg'},
+    { 'id': '6', 'url':'ippo.png'},
 ]
 
-const container = document.querySelector('#container-items');
-
+const containerItems = document.querySelector('#container-items');
 const loadImages = (images, container) =>{
     images.forEach (image => {
         container.innerHTML += `
-            <div class='item'>
-                <img src='${image.url}'
-            </div>
+        <div class='item'>
+        <img src='${image.url}'
+        </div>
         `
     })
 }
 
-loadImages(images, container)
+loadImages(images, containerItems)
 
+let items = document.querySelectorAll('.item'); //seleciona todas as imagens
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore(lastItem, items[0])
+    items = document.querySelectorAll('.item');
+}
+
+const btnNext = document.querySelector('#next').addEventListener('click', next);
+const btnPrevious = document.querySelector('#previous').addEventListener('click', previous);

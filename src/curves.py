5a39fb50c0e4e82f5546f08c21267bb48d699846
filipe_sales <49558962+filipe-jsales@users.noreply.github.com>
2023-05-25 13:12:37 -0400

    feat: add class-level documentation for curves

--- src/curves.py
+++ src/curves.py
@@ -1,16 +1,37 @@
 from src.rasterization import Rasterization
 from src.bresenham import Bresenham
 
-'''
-para construir uma curva são calculados points (que fazem parte da curva)
-a partir dos pts de controle. Em seguida, os points calculados são
-conectados com o algoritmo de Bresenham.
-
-a variavel n define quantos points serão calculados
-'''
-
-
 class Curves(Rasterization):
+    """
+    A class that represents curves and performs rasterization using the Bresenham algorithm to draw the curves.
+
+    This class inherits from the Rasterization class.
+
+    Attributes:
+        n (int): The number of points to interpolate on the curve.
+        control_pts (list): A list of control points defining the curve.
+
+        points (list): A list to store the interpolated points on the curve.
+        output_points (list): A list to store the output points after rasterization.
+
+    Methods:
+        __init__(self, n, control_pts):
+            Initializes a Curves object.
+
+        casteljau(self, t, control_points):
+            Performs the Casteljau's algorithm to interpolate a point on the curve at parameter t.
+
+        join_points(self):
+            Connects the interpolated points on the curve using the Bresenham algorithm.
+
+    Example usage:
+        # Create an instance of the Curves class
+        control_points = [(0, 0), (2, 5), (5, 3)]
+        curves = Curves(10, control_points)
+
+        # Get the output points after rasterization
+        output_points = curves.output_points
+    """
     def __init__(self, n, control_pts: list):
         super().__init__([control_pts])
 
@@ -25,6 +46,16 @@ class Curves(Rasterization):
         self.join_points()
 
     def casteljau(self, t, control_points):
+        """
+        Performs the Casteljau's algorithm to interpolate a point on the curve at parameter t.
+
+        Args:
+            t (float): The parameter value at which to interpolate the point on the curve.
+            control_points (list): A list of control points defining the curve.
+
+        Returns:
+            list: The interpolated point on the curve as a list [x, y].
+        """
         pts = []
         for p in control_points:
             pts.append(list(p))
@@ -37,6 +68,9 @@ class Curves(Rasterization):
         return [int(pts[0][0]), int(pts[0][1])]
 
     def join_points(self):
+        """
+        Connects the interpolated points on the curve using the Bresenham algorithm.
+        """
         for x in range(len(self.points) - 1):
             line = Bresenham(self.points[x], self.points[x + 1])
 

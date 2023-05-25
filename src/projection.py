dbd671e1b03609d50704679d4abccd2ca7a0b206
filipe_sales <49558962+filipe-jsales@users.noreply.github.com>
2023-05-25 13:25:22 -0400

    feat: add class-level documentation for projection

--- src/projection.py
+++ src/projection.py
@@ -5,17 +5,40 @@ import numpy as np
 
 
 class Projection(Rasterization):
+    """
+    A class representing a Projection, which performs various projection operations on a set of input points.
+
+    This class inherits from the Rasterization class and provides methods for projection and perspective transformation
+    on the input points. It utilizes the Polyline and Bresenham classes for generating the output points.
+
+    Attributes:
+        input_points (list): The list of input points to be projected.
+        shift (float): The shift value to be added to the z-coordinate of the input points.
+    """
+
     def __init__(self, input_points, shift):
+        """
+        Initializes a Projection object with the given input points and shift value.
+
+        Args:
+            input_points (list): The list of input points to be projected.
+            shift (float): The shift value to be added to the z-coordinate of the input points.
+        """
         for coordinate in input_points:
             coordinate[2] += shift
         super().__init__(input_points)
 
     def project(self):
+        """
+        Performs a projection operation on the input points.
+
+        The method projects the input points onto the xy-plane by performing matrix multiplication using a projection
+        matrix. The resulting points are stored in the output_points attribute.
+        """
         for point in self.input_points:
             point.append(1)
 
-        matrix_projection = [[0 for i in range(
-            len(self.input_points[0]))] for i in range(len(self.input_points[0]))]
+        matrix_projection = [[0 for i in range(len(self.input_points[0]))] for i in range(len(self.input_points[0]))]
 
         missed_diagonal = 2
         x_output = 0
@@ -27,34 +50,38 @@ class Projection(Rasterization):
 
         for point in self.input_points:
             projection = np.dot(matrix_projection, point)
-            self.output_points.append(
-                [projection[x_output], projection[y_output]])
+            self.output_points.append([projection[x_output], projection[y_output]])
 
-        self.output_points = Polyline(
-            self.output_points, close=True).output_points
+        self.output_points = Polyline(self.output_points, close=True).output_points
 
     def perspective(self, dist):
+        """
+        Performs a perspective transformation on the input points.
+
+        The method applies perspective transformation to the input points using a perspective matrix. The resulting
+        points are stored in the output_points attribute. Additionally, the method adds extra points to create a
+        perspective view effect.
+
+        Args:
+            dist (float): The distance parameter for the perspective transformation.
+        """
         for point in self.input_points:
             point.append(1)
 
-        matrix_perspective = [[0 for i in range(
-            len(self.input_points[0]))] for i in range(len(self.input_points[0]))]
+        matrix_perspective = [[0 for i in range(len(self.input_points[0]))] for i in range(len(self.input_points[0]))]
 
         for i in range(0, len(self.input_points[0])):
             if i != len(self.input_points[0])-1:
                 matrix_perspective[i][i] = dist
 
-        matrix_perspective[len(matrix_perspective) -
-                           1][len(matrix_perspective)-2] = 1
+        matrix_perspective[len(matrix_perspective) - 1][len(matrix_perspective) - 2] = 1
 
         for point in self.input_points:
             projection = np.dot(matrix_perspective, point)
             projection = np.multiply(projection, 1/point[2])
-            self.output_points.append(
-                [round(projection[0]), round(projection[1])])
+            self.output_points.append([round(projection[0]), round(projection[1])])
 
-        self.output_points = Polyline(
-            self.output_points, close=True).output_points
+        self.output_points = Polyline(self.output_points, close=True).output_points
 
         uniques = []
 

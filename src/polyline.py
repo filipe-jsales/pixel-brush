939749256d7fcfc8852878287baa9966564c1a67
filipe_sales <49558962+filipe-jsales@users.noreply.github.com>
2023-05-25 13:23:40 -0400

    feat: add class-level documentation for polyline

--- src/polyline.py
+++ src/polyline.py
@@ -3,7 +3,26 @@ from src.bresenham import Bresenham
 
 
 class Polyline(Rasterization):
+    """
+    A class representing a Polyline, which is a series of connected line segments.
+
+    This class inherits from the Rasterization class and provides methods to generate the set of points that approximate
+    the Polyline using the Bresenham's line algorithm.
+
+    Attributes:
+        points (list): The list of points defining the Polyline.
+        close (bool): Indicates whether the Polyline is closed (forms a closed shape) or open (ends without connecting
+            back to the starting point).
+    """
+
     def __init__(self, points: list, close=False):
+        """
+        Initializes a Polyline object with the given points.
+
+        Args:
+            points (list): The list of points defining the Polyline.
+            close (bool, optional): Indicates whether the Polyline is closed. Defaults to False.
+        """
         super().__init__(points)
 
         if close:

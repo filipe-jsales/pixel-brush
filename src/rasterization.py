eefae7a55c4fb44e0c746a5794bb8e3a13e2fbf2
filipe_sales <49558962+filipe-jsales@users.noreply.github.com>
2023-05-25 13:26:07 -0400

    feat: add class-level documentation for rasterization

--- src/rasterization.py
+++ src/rasterization.py
@@ -1,6 +1,21 @@
-
-
 class Rasterization:
+    """
+    A class representing a Rasterization, which converts input points into a rasterized representation.
+
+    The Rasterization class provides a base class for performing rasterization operations on a set of input points.
+    It defines the input points and output points attributes.
+
+    Attributes:
+        input_points (list): The list of points of the input figure.
+        output_points (list): The list of points that compose the rasterization.
+    """
+
     def __init__(self, input_points):
-        self.input_points = input_points  # list of points of the input figure
-        self.output_points = []         # list of points that compose the rasterization
+        """
+        Initializes a Rasterization object with the given input points.
+
+        Args:
+            input_points (list): The list of points of the input figure.
+        """
+        self.input_points = input_points
+        self.output_points = []
